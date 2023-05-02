f5455688f81e941f5708417ff14a9474fd8eed19
soywiz <soywiz@gmail.com>
2023-05-02 20:17:18 +0200

    Korge 4.0.0-rc4

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -3,7 +3,7 @@ import korlibs.korge.gradle.*
 plugins {
     //alias(libs.plugins.korge)
     //id("com.soywiz.korge") version "999.0.0.999"
-    id("com.soywiz.korge") version "4.0.0-rc"
+    id("com.soywiz.korge") version "4.0.0-rc4"
 }
 
 korge {
