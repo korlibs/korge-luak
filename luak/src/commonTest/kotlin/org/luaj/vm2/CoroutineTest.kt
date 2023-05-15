7e87744875f3595f74970917523b96ba74e35b7e
soywiz <soywiz@gmail.com>
2023-05-15 18:18:00 +0200

    Add CoroutineTest to the commonTest sourceSet

--- luak/src/commonTest/kotlin/org/luaj/vm2/CoroutineTest.kt
+++ luak/src/commonTest/kotlin/org/luaj/vm2/CoroutineTest.kt
@@ -1,8 +1,12 @@
 package org.luaj.vm2
 
+import org.luaj.test.suspendTest
 import org.luaj.vm2.compiler.LuaC
+import org.luaj.vm2.io.ByteArrayLuaBinOutput
+import org.luaj.vm2.io.LuaWriterBinOutput
 import org.luaj.vm2.lib.*
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class CoroutineTest {
     @Test
@@ -24,5 +28,58 @@ class CoroutineTest {
             
             print(co)
         """.trimIndent()).call()
+    }
+
+
+    @Test
+    fun testCoroutines() = suspendTest {
+        val stdout = ByteArrayLuaBinOutput()
+        val globals = Globals().apply {
+            STDOUT = LuaWriterBinOutput(stdout)
+            load(BaseLib())
+            load(PackageLib())
+            load(Bit32Lib())
+            load(TableLib())
+            load(StringLib())
+            load(CoroutineLib())
+            LoadState.install(this)
+            LuaC.install(this)
+        }
+        val code = globals.load(
+            // language=lua
+            """
+            co = coroutine.create(function ()
+               for i=1,4 do
+                 print("co", i)
+                 coroutine.yield(i, i + 1)
+               end
+               return "completed"
+             end)
+            
+            for i=1,6 do
+                local code, res = coroutine.resume(co)
+                print(code, res)
+            end
+            print("ENDED!")
+        """.trimIndent())
+        //println("code=$code")
+        code.callSuspend()
+
+        assertEquals(
+            """
+                co	1
+                true	1
+                co	2
+                true	2
+                co	3
+                true	3
+                co	4
+                true	4
+                true	completed
+                false	cannot resume dead(4) coroutine
+                ENDED!
+            """.trimIndent().trim(),
+            stdout.toByteArray().decodeToString().trim()
+        )
     }
 }
