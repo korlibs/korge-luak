96460498ee15b742e9c1211b8a2d5b34baada3c7
Der_s <isthatder@gmail.com>
2023-12-30 11:13:41 -0700

    Fix Jitpack not compiling (#14)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import com.android.build.gradle.internal.lint.AndroidLintAnalysisTask
 import korlibs.korge.gradle.*
 
 plugins {
@@ -10,8 +11,18 @@ korge {
     targetJvm()
     targetJs()
     targetIos()
-    targetAndroidDirect()
+    if (System.getenv("JITPACK") != "true") {
+        targetAndroidDirect()
+    }
     serializationJson()
+}
+
+afterEvaluate {
+    if (System.getenv("JITPACK") != "true") {
+        tasks.getByName<AndroidLintAnalysisTask>("lintVitalAnalyzeRelease") {
+            dependsOn(tasks.getByName("jvmProcessResources"))
+        }
+    }
 }
 
 dependencies {
